fac0d972de531e45b51ea16c1c6dd78b040f33a5
LuminousBinary <boolwrld@gmail.com>
2024-02-13 16:15:07 +0000

    completerd the menu tree components using a custom data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ImageSlider from "./components/image-slider";
 import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -21,7 +23,10 @@ function App() {
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={8} page={2}/> */}
 
       {/* load more data component */}
-      <LoadMoreData url={'https://dummyjson.com/products'}/>
+      {/* <LoadMoreData url={'https://dummyjson.com/products'}/> */}
+
+      {/* a tree view list menu item // tree view componemnt // navigation menu*/}
+      <TreeView menus={menus} />
     </div>
   );
 }
